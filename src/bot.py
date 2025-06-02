0e680756f75b919b4405699077e92a8895f080ea
Silvano Hu <silvahu02@gmail.com>
2025-06-02 17:01:33 +0200

    Refactor: Organize main bot setup by importing handlers and scheduler

--- src/bot.py
+++ src/bot.py
@@ -1,36 +1,47 @@
+"""
+Main Telegram bot application.
+"""
 from dotenv import load_dotenv
 import os
 import datetime
 
-from telegram import ForceReply, Update
-from telegram.ext import Application, CommandHandler, ContextTypes, MessageHandler, filters, JobQueue
+from telegram import Update
+from telegram.ext import Application, CommandHandler, MessageHandler, filters
 
-import jmdict
-
+from handlers import start, help_command, random_entry, subscribe, unsubscribe
+from scheduler import send_random_entry_to_all
 
 load_dotenv()
 
 
-def main() -> None:
-    """Start the bot."""
-    # Create the Application and pass it your bot's token.
-    application = Application.builder().token(os.getenv("TELEGRAM_BOT_TOKEN")).build()
-
-    job_queue = application.job_queue
-    job_queue.run_daily(send_random_entry_to_all,
-                        time=datetime.time(hour=14, minute=10, second=0),
-                        days=(0, 1, 2, 3, 4, 5, 6))
-
-    application.add_handler(CommandHandler("subscribe", subscribe))
-    application.add_handler(CommandHandler("unsubscribe", unsubscribe))
-
-    # on different commands - answer in Telegram
+def setup_handlers(application: Application) -> None:
+    """Configure all command and message handlers."""
+    # Command handlers
     application.add_handler(CommandHandler("start", start))
     application.add_handler(CommandHandler("help", help_command))
     application.add_handler(CommandHandler("random", random_entry))
+    application.add_handler(CommandHandler("subscribe", subscribe))
+    application.add_handler(CommandHandler("unsubscribe", unsubscribe))
 
-    # on non command i.e message - echo the message on Telegram
-    application.add_handler(MessageHandler(filters.TEXT & ~filters.COMMAND, echo))
+
+def setup_scheduler(application: Application) -> None:
+    """Configure scheduled jobs."""
+    job_queue = application.job_queue
+    job_queue.run_daily(
+        send_random_entry_to_all,
+        time=datetime.time(hour=15, minute=5, second=0),
+        days=(0, 1, 2, 3, 4, 5, 6)  # All days of the week
+    )
+
+
+def main() -> None:
+    """Start the bot."""
+    # Create the Application
+    application = Application.builder().token(os.getenv("TELEGRAM_BOT_TOKEN")).build()
+
+    # Setup handlers and scheduler
+    setup_handlers(application)
+    setup_scheduler(application)
 
     # Run the bot until the user presses Ctrl-C
     application.run_polling(allowed_updates=Update.ALL_TYPES)
