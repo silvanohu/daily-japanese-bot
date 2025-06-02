a2f16e31eea89062647d71400d53c21a3b6ea79b
Silvano Hu <silvahu02@gmail.com>
2025-06-02 16:59:27 +0200

    Refactor: Extract scheduled job function into a separate module

--- src/bot.py
+++ src/bot.py
@@ -11,16 +11,6 @@ import jmdict
 load_dotenv()
 
 
-async def send_random_entry_to_all(context: ContextTypes.DEFAULT_TYPE) -> None:
-    """Send a random entry to all registered chats."""
-    entry = jmdict.get_random_jmdict_entry()
-    entry = jmdict.format_jmdict_entry(entry)
-
-    if 'chats' in context.bot_data:
-        for chat_id in context.bot_data['chats']:
-            await context.bot.send_message(chat_id=chat_id, text=entry)
-
-
 def main() -> None:
     """Start the bot."""
     # Create the Application and pass it your bot's token.
